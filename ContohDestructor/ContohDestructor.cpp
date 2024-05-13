645ce2ae10c6ac755841ad286992f6d2a19e2490
Yusuf Hidayat <sufhidayat9@gmail.com>
2024-05-13 15:56:05 +0700

    pendefinisian member function cetakData

--- ContohDestructor/ContohDestructor.cpp
+++ ContohDestructor/ContohDestructor.cpp
@@ -24,3 +24,11 @@ angka::~angka() {
 	delete[]arr;
 	cout << "Alamat array sudah dilepaskan" << endl;
 }
+
+void angka::cetakData() {
+	for (int i = 1; i <= panjang, i++) {
+		cout << i << " = ";
+		cin >> arr[i];
+	}
+	cout << endl;
+}
