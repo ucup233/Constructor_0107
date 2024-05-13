cebcc53d5f811d9e270b88d453cc1f92af1acdf7
Yusuf Hidayat <sufhidayat9@gmail.com>
2024-05-13 10:54:51 +0700

    class mahasiswa dengan member akses modifier public

--- StaticEx2/StaticEx2.cpp
+++ StaticEx2/StaticEx2.cpp
@@ -4,4 +4,15 @@ using namespace std;
 class mahasiswa {
 private:
 	static int nim;
+public:
+	int id;
+	string nama;
+
+	void setID();
+	void printAll();
+
+	static void setNim(int pNim) { nim = pNim; }
+	static void getNim() { return nim; }
+
+	mahasiswa(string pnama) :nama(pnama) { setID(); }
 };
