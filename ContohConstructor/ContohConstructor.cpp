546df955842c3e3ff791b036d7b83876ba858ecd
Yusuf Hidayat <sufhidayat9@gmail.com>
2024-05-13 11:48:51 +0700

    int main

--- ContohConstructor/ContohConstructor.cpp
+++ ContohConstructor/ContohConstructor.cpp
@@ -1,17 +1,54 @@
 #include <iostream>
 using namespace std;
 
-class mahasiswa {
+class Mahasiswa {
 public:
 	int nim;
 	string nama;
 
-	mahasiswa() {
+public:
+	Mahasiswa() {
 		nim = 0;
 		nama = "";
 	};
-	mahasiswa(int);
-	mahasiswa(string);
-	mahasiswa(int iNim, string iNama);
-	void cetak();
+	Mahasiswa(int iNim) {
+		nim = iNim;
+	};
+	Mahasiswa(string iNama) {
+		nama = iNama;
+	};
+	Mahasiswa(int cNim, string cNama) {
+	};
+	void cetak() {
+		cout << endl << "Nim = " << nim << endl;
+		cout << "Nama = " << nama << endl;
+	};
 };
+
+Mahasiswa::Mahasiswa(int iNim) {
+	nim = iNim;
+}
+
+Mahasiswa::Mahasiswa(string iNama) {
+	nim = 0;
+	nama = iNama;
+}
+
+Mahasiswa::Mahasiswa(int cNim, string cNama) {
+	nim = cNim;
+	nama = cNama;
+}
+
+
+
+int main() {
+	Mahasiswa mhs1;
+	Mahasiswa mhs2(10);
+	Mahasiswa mhs3("Samsul");
+	Mahasiswa mhs4(11, "Indra");
+
+	mhs1.cetak();
+	mhs2.cetak();
+	mhs3.cetak();
+	mhs4.cetak();
+}
