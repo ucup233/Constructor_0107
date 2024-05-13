2df0d50895f7b986394175e2bba2fb8844d5c7d5
Yusuf Hidayat <sufhidayat9@gmail.com>
2024-05-13 10:58:33 +0700

    set value nim dari class mahasiswa

--- StaticEx2/StaticEx2.cpp
+++ StaticEx2/StaticEx2.cpp
@@ -3,16 +3,19 @@ using namespace std;
 
 class mahasiswa {
 private:
-	static int nim;
+	static long long int nim;
 public:
-	int id;
+	long long int id;
 	string nama;
 
 	void setID();
 	void printAll();
 
-	static void setNim(int pNim) { nim = pNim; }
-	static void getNim() { return nim; }
+	static void setNim(long long int pNim) { nim = pNim; }
+	static long long int getNim() { return nim; }
 
 	mahasiswa(string pnama) :nama(pnama) { setID(); }
 };
+
+long long int mahasiswa::nim = 20230140107;
+
